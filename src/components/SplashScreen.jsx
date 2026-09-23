ce3dc3e84a53a1a6960243aff8db470c38f1d550
Voldi Bill Paulo Ngangu <aagustpro@gmail.com>
2026-09-23 16:09:33 +0100

    fix: remove artificial splash image boundary

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -24,60 +24,29 @@ export default function SplashScreen({ onContinue }) {
         aria-hidden="true"
       >
         <defs>
-          <clipPath id="pedeja-luanda-ribbon" clipPathUnits="userSpaceOnUse">
-            <path
-              d="M0 248
-                 C55 250 94 284 126 350
-                 C165 431 213 505 280 548
-                 C327 578 378 594 430 602
-                 L430 932
-                 L0 932 Z"
-            />
-          </clipPath>
-
-          <linearGradient id="pedeja-city-tint" x1="0" y1="0" x2="0.85" y2="1">
-            <stop offset="0" stopColor="#6D28D9" stopOpacity="0.30" />
-            <stop offset="0.42" stopColor="#5B21B6" stopOpacity="0.50" />
-            <stop offset="1" stopColor="#2E1065" stopOpacity="0.72" />
-          </linearGradient>
-
-          <linearGradient id="pedeja-ribbon-edge" x1="0" y1="0" x2="1" y2="0">
-            <stop offset="0" stopColor="#6D28D9" stopOpacity="0.88" />
-            <stop offset="0.72" stopColor="#6D28D9" stopOpacity="0.12" />
-            <stop offset="1" stopColor="#6D28D9" stopOpacity="0" />
+          <linearGradient id="pedeja-city-tint" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="0" stopColor="#4C1D95" stopOpacity="0.92" />
+            <stop offset="0.38" stopColor="#4C1D95" stopOpacity="0.42" />
+            <stop offset="1" stopColor="#2E1065" stopOpacity="0.58" />
           </linearGradient>
         </defs>
 
         <image
           href={LUANDA_IMAGE}
           x="0"
-          y="210"
+          y="0"
           width="430"
-          height="722"
+          height="932"
           preserveAspectRatio="xMidYMid slice"
-          clipPath="url(#pedeja-luanda-ribbon)"
-          style={{ filter: 'saturate(.72) contrast(1.05) brightness(.72)' }}
+          style={{ filter: 'saturate(.78) contrast(1.04) brightness(.76)' }}
         />
 
-        <path
-          d="M0 248
-             C55 250 94 284 126 350
-             C165 431 213 505 280 548
-             C327 578 378 594 430 602
-             L430 932
-             L0 932 Z"
+        <rect
+          x="0"
+          y="0"
+          width="430"
+          height="932"
           fill="url(#pedeja-city-tint)"
-        />
-
-        <path
-          d="M0 248
-             C55 250 94 284 126 350
-             C165 431 213 505 280 548
-             C327 578 378 594 430 602"
-          fill="none"
-          stroke="url(#pedeja-ribbon-edge)"
-          strokeWidth="22"
-          strokeLinecap="round"
         />
       </svg>
 
