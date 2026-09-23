7090724ee01b68cd64a926e667e7a34c99c4d3c6
Voldi Bill Paulo Ngangu <aagustpro@gmail.com>
2026-09-23 08:46:45 +0100

    fix: point top-up modal to provider-neutral payment component

--- src/components/customer/TopUpModal.jsx
+++ src/components/customer/TopUpModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { X, Check, Receipt } from 'lucide-react';
 import { useApp } from '../../context/AppContext';
-import PaymentQR from '../PaymentQR';
+import PaymentQR from '../PromptPayQR';
 
 export default function TopUpModal() {
   const {
