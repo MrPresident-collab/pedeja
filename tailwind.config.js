f6429dcd42c8ef960e48e80862172d576f8ae740
Voldi Bill Paulo Ngangu <aagustpro@gmail.com>
2026-09-23 07:47:57 +0100

    chore: rebrand BoomRider surface to Pedeja

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          50: '#F5F3FF',
+          100: '#EDE9FE',
+          200: '#DDD6FE',
+          300: '#C4B5FD',
+          400: '#A78BFA',
+          500: '#7C3AED',
+          600: '#6D28D9',
+          700: '#5B21B6',
+          800: '#4C1D95',
+          900: '#2E1065'
+        }
+      }
+    }
   },
   plugins: [],
 }
